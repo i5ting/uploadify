979d3862f19fdd10e09f279a6720503b05a2811e
i5ting <i5ting@126.com>
2015-11-11 20:17:18 +0800

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var express  = require('express');
+var app      = express();
+var path     = require('path');
+var open     = require("open");
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, '/.')));
+
+var uploadify = require('.')
+
+uploadify(app,{
+  path:'/fileupload'
+  fileKey:'myfile'
+  multer:{ dest: 'uploads/' }
+});
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    
+  })
+})
+// 随机端口3000 - 10000 之间
+app.listen(5024)
+
+open("http://127.0.0.1:5024");
+
---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var multer  = require('multer');
+
+module.exports = function (app, cfg){
+  // console.log(cfg);
+  var upload = multer(cfg.multer);
+  var upload_action = require('./upload')(cfg); 
+  
+  if(!cfg.path){
+    cfg.path = '/fileupload';
+  }
+  
+  if(!cfg.fileKey){
+    cfg.fileKey = 'myfile';
+  }
+  
+  app.post(cfg.path, upload.array(cfg.fileKey), function (req, res, next) {
+    res.status(200).json(req.files)
+  })
+  
+}
