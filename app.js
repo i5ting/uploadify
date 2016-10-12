ddc34e9391d3b97093b5138d84318a822acf7d99
i5ting <i5ting@126.com>
2016-10-13 03:47:24 +0800

    lazycommmit, please ignore

--- app.js
+++ app.js
@@ -17,23 +17,14 @@ app.use(views(__dirname + '/views', {
 app.use (bodyParser());
 app.use ( static(__dirname + '/.'));
 
-
 var mount_uploadify = require('.')
 
 mount_uploadify (app, {
-  debug:true,
-  path:'/fileupload',
-  fileKey:'myfile',
-  multer:{ dest: 'uploads/' },
-	qn:{
-		accessKey: 'PqLfYe68_HKhnCL0qszXD4xRFj57U8cnBASJN0x7',
-		secretKey: 'KFjdvN4aOmqQG_lV2YViY7tHPZOKROA8cmK7J5CH',
-		bucket: 'mengxiaoban',
-		origin: 'http://{bucket}.u.qiniudn.com',
-		// timeout: 3600000, // default rpc timeout: one hour, optional
-		// if your app outside of China, please set `uploadURL` to `http://up.qiniug.com/`
-		// uploadURL: 'http://up.qiniu.com/',
-	}
+  debug: true,
+  path: '/fileupload',
+  fileKey: 'myfile',
+  multer: { dest: 'uploads/' },
+	qn: reqire('./qn')
 });
 
 
---
+++ qn.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+	accessKey: 'xxx',
+	secretKey: 'yyy',
+	bucket: 'zzz',
+	origin: 'http://{bucket}.u.qiniudn.com',
+	// timeout: 3600000, // default rpc timeout: one hour, optional
+	// if your app outside of China, please set `uploadURL` to `http://up.qiniug.com/`
+	// uploadURL: 'http://up.qiniu.com/',
+}
